845cd46c553d50060ca09238a5eaac30e42249f2
mks2906 <manoj.singh@clear2pay.com>
2015-07-18 07:20:23 +0530

    creating code base

---
+++ copywar.bat
@@ -0,0 +1,4 @@
+del /Q D:\Softwares\apache-tomcat-7.0.47\webapps\*.*
+rmdir /Q /S D:\Softwares\apache-tomcat-7.0.47\webapps\adminportal
+rmdir /Q /S D:\Softwares\apache-tomcat-7.0.47\webapps\test
+copy D:\MyWork\CinchWallet\codebase\adminportal\target\adminportal.war D:\Softwares\apache-tomcat-7.0.47\webapps\
